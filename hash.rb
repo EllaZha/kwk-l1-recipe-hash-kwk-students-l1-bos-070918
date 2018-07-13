98c464302ec28b3a21bb4f2265f972fdbe86f757
Ella Zhang <eyanchizhang@gmail.com>
2018-07-13 18:36:10 +0000

    automatically backed up by learn

---
+++ hash.rb
@@ -0,0 +1,12 @@
+perfect_10_recipe = {
+      :almond_flour => "2 cups",
+      :gluten_freewhole_oats => "1 cup",
+      :kosher_salt => "1/2 tea spoon",
+      :baking_powder  => "1/2 tea spoon",
+      :baking_soda => "1/4 tea spoon",
+      :xanthan_Gum => "1/2 tea spoon",
+      :slivered_almonds => "1/4 cup",
+      :mini_dark_chocolate_chips => "3/4 cup",
+      :olive_oil => "1/2 cup",
+      :agave => "1/4 cup"
+    }
